01d6f4e57a3bb9a3ffb4e282b6cfd0dc345d789a
Cixtor <cixtords@gmail.com>
2017-02-13 09:00:00 -0600

    Modify entire code base to refactor the JSON decoder

--- client.go
+++ client.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
-	"os"
 	"strings"
 )
 
-type SiteCheck struct{}
+type Scanner struct {
+	Domain    string
+	FromCache bool
+	Report    Result
+}
 
 type Result struct {
 	Scan            map[string][]string
@@ -35,55 +39,69 @@ type InfoWarning struct {
 	Warn [][]string
 }
 
-func (s *SiteCheck) Scan(domain string) []byte {
-	var url string = fmt.Sprintf("https://sitecheck.sucuri.net/?fromwp=2&clean=1&json=1&scan=%s", domain)
-	req, err := http.NewRequest("GET", url, nil)
-	client := &http.Client{}
+func NewScanner(domain string) *Scanner {
+	return &Scanner{Domain: domain}
+}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0 (KHTML, like Gecko) Safari/537.36")
-	req.Header.Add("Accept-Language", "end-US,en")
-	req.Header.Add("Accept", "application/json")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("DNT", "1")
+func (s *Scanner) URL() string {
+	urlStr := service
+
+	urlStr += "?json=1"
+	urlStr += "&fromwp=2"
+
+	if !s.FromCache {
+		/* get fresh results */
+		urlStr += "&clean=1"
+	}
+
+	urlStr += "&scan=" + s.Domain
+
+	return urlStr
+}
+
+func (s *Scanner) UseCachedResults() {
+	s.FromCache = true
+}
+
+func (s *Scanner) Request() (io.Reader, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", s.URL(), nil)
 
 	if err != nil {
-		fmt.Printf("Error request initialization: %s\n", err)
-		os.Exit(1)
+		return nil, err
 	}
+
+	req.Header.Add("DNT", "1")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Accept-Language", "end-US,en")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (KHTML, like Gecko) Safari/537.36")
 
 	resp, err := client.Do(req)
 
 	if err != nil {
-		fmt.Printf("Error request execution: %s\n", err)
-		os.Exit(1)
+		return nil, err
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
 
-	if err != nil {
-		fmt.Printf("Error response data: %s\n", err)
-		os.Exit(1)
-	}
+	var buf bytes.Buffer
+	(&buf).ReadFrom(resp.Body)
 
-	return body
+	return &buf, nil
 }
 
-func (s *SiteCheck) Data(domain string) Result {
-	var result Result
-	var response []byte = s.Scan(domain)
-	err := json.Unmarshal(response, &result)
+func (s *Scanner) Scan() error {
+	reader, err := s.Request()
 
 	if err != nil {
-		fmt.Printf("JSON structure not supported: %s\n", err)
-		fmt.Println(string(response))
-		os.Exit(1)
+		return err
 	}
 
-	return result
+	return json.NewDecoder(reader).Decode(&s.Report)
 }
 
-func (s *SiteCheck) Justify(text string) string {
+func (s *Scanner) Justify(text string) string {
 	var chunk int = 97
 	var lines int = 10
 	var limit int = lines * chunk
@@ -117,53 +135,56 @@ func (s *SiteCheck) Justify(text string) string {
 	return final
 }
 
-func (s *SiteCheck) Print(result Result) {
-	fmt.Println("\033[48;5;008m >>> Website Information <<< \033[0m")
-	for key, value := range result.Scan {
+func (s *Scanner) Print() {
+	fmt.Print("\r") /* clear previous loading message */
+	fmt.Print("\033[48;5;008m @ Website Information \033[0m")
+	fmt.Print(strings.Repeat("\x20", len(s.Domain)) + "\n")
+	for key, value := range s.Report.Scan {
 		fmt.Printf(" \033[1;95m%s:\033[0m %s\n", key, strings.Join(value, ",\x20"))
 	}
-	for _, values := range result.System {
+	for _, values := range s.Report.System {
 		for _, value := range values {
 			fmt.Printf(" \033[0;2m%s\033[0m\n", value)
 		}
 	}
 
-	if len(result.WebApp.Warn) > 0 ||
-		len(result.WebApp.Info) > 0 ||
-		len(result.WebApp.Version) > 0 ||
-		len(result.WebApp.Notice) > 0 {
+	if len(s.Report.WebApp.Warn) > 0 ||
+		len(s.Report.WebApp.Info) > 0 ||
+		len(s.Report.WebApp.Version) > 0 ||
+		len(s.Report.WebApp.Notice) > 0 {
 		fmt.Println()
-		fmt.Println("\033[48;5;008m >>> Application Details <<< \033[0m")
-		for _, value := range result.WebApp.Warn {
+		fmt.Println("\033[48;5;008m @ Application Details \033[0m")
+		for _, value := range s.Report.WebApp.Warn {
 			fmt.Printf(" %s\n", value)
 		}
-		for _, values := range result.WebApp.Info {
+		for _, values := range s.Report.WebApp.Info {
 			fmt.Printf(" %s \033[0;2m%s\033[0m\n", values[0], values[1])
 		}
-		for _, value := range result.WebApp.Version {
+		for _, value := range s.Report.WebApp.Version {
 			fmt.Printf(" %s\n", value)
 		}
-		for _, value := range result.WebApp.Notice {
+		for _, value := range s.Report.WebApp.Notice {
 			fmt.Printf(" %s\n", value)
 		}
 	}
 
 	// Print security recommendations.
-	if len(result.Recommendations) > 0 {
+	if len(s.Report.Recommendations) > 0 {
 		fmt.Println()
-		fmt.Println("\033[48;5;068m >>> Recommendations <<< \033[0m")
-		for _, values := range result.Recommendations {
-			fmt.Printf(" \033[0;94m\u2022\033[0m %s\n", values[0])
-			fmt.Printf("   %s\n", values[1])
-			fmt.Printf("   %s\n", values[2])
+		fmt.Println("\033[48;5;068m @ Recommendations \033[0m")
+		for _, values := range s.Report.Recommendations {
+			fmt.Print(" \033[0;94m\u2022\033[0m")
+			fmt.Print(" \033[0;1m" + values[0] + "\033[0m\n")
+			fmt.Print("   " + values[1] + "\n")
+			fmt.Print("   " + values[2] + "\n")
 		}
 	}
 
 	// Print outdated software information.
-	if len(result.OutdatedScan) > 0 {
+	if len(s.Report.OutdatedScan) > 0 {
 		fmt.Println()
-		fmt.Println("\033[48;5;068m >>> OutdatedScan <<< \033[0m")
-		for _, values := range result.OutdatedScan {
+		fmt.Println("\033[48;5;068m @ OutdatedScan \033[0m")
+		for _, values := range s.Report.OutdatedScan {
 			fmt.Printf(" \033[0;94m\u2022\033[0m %s\n", values[0])
 			fmt.Printf("   %s\n", values[1])
 			fmt.Printf("   %s\n", values[2])
@@ -171,37 +192,37 @@ func (s *SiteCheck) Print(result Result) {
 	}
 
 	// Print links, iframes, and local/external javascript files.
-	for key, values := range result.Links {
+	for key, values := range s.Report.Links {
 		fmt.Println()
-		fmt.Printf("\033[48;5;097m >>> Links %s <<< \033[0m\n", key)
+		fmt.Printf("\033[48;5;097m @ Links %s \033[0m\n", key)
 		for _, value := range values {
 			fmt.Printf(" %s\n", value)
 		}
 	}
 
 	// Print blacklist status information.
-	if len(result.Blacklist.Warn) > 0 || len(result.Blacklist.Info) > 0 {
+	if len(s.Report.Blacklist.Warn) > 0 || len(s.Report.Blacklist.Info) > 0 {
 		fmt.Println()
 		var blacklist_color string = "034"
-		if len(result.Blacklist.Warn) > 0 {
+		if len(s.Report.Blacklist.Warn) > 0 {
 			blacklist_color = "161"
 		}
-		fmt.Printf("\033[48;5;%sm >>> Blacklist Status <<< \033[0m\n", blacklist_color)
-		for _, values := range result.Blacklist.Warn {
+		fmt.Printf("\033[48;5;%sm @ Blacklist Status \033[0m\n", blacklist_color)
+		for _, values := range s.Report.Blacklist.Warn {
 			fmt.Printf(" \033[0;91m\u2718\033[0m %s\n", values[0])
 			fmt.Printf("   %s\n", values[1])
 		}
-		for _, values := range result.Blacklist.Info {
+		for _, values := range s.Report.Blacklist.Info {
 			fmt.Printf(" \033[0;92m\u2714\033[0m %s\n", values[0])
 			fmt.Printf("   %s\n", values[1])
 		}
 	}
 
 	// Print malware payload information.
-	if len(result.Malware.Warn) > 0 {
+	if len(s.Report.Malware.Warn) > 0 {
 		fmt.Println()
-		fmt.Println("\033[48;5;161m >>> Malware Payloads <<< \033[0m")
-		for _, values := range result.Malware.Warn {
+		fmt.Println("\033[48;5;161m @ Malware Payloads \033[0m")
+		for _, values := range s.Report.Malware.Warn {
 			fmt.Printf(" \033[0;91m\u2022\033[0m %s\n", values[0])
 			fmt.Printf("%s", s.Justify(values[1]))
 		}
